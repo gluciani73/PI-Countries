b9ab5f74f909961696b713f2cc0d74f3d779038f
Guillermo <guillermo.luciani@gmail.com>
2022-08-13 20:49:09 -0300

    20220813-2050

--- api/src/db.js
+++ api/src/db.js
@@ -36,8 +36,8 @@ const { Country, Activity } = sequelize.models;
 // Product.hasMany(Reviews);
 
 
-Country.belongsToMany(Activity, { through: 'Country_Activity' });
-Activity.belongsToMany(Country, { through: 'Country_Activity' });
+Country.belongsToMany(Activity, { through: 'country_activity' });
+Activity.belongsToMany(Country, { through: 'country_activity' });
 
 
 
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,41 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
-
+const Sequelize = require('sequelize')
 const router = Router();
+const axios = require('axios');
+// Traigo los modelos de db
+// const { Activity } = require('')
+const { Activity, Country, country_activity } = require('../db')
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+
+// Primero vamos a llenar la DB con info de la api externa restcountries 
+
+const arrInfoCountries = async () => {
+    const arrCountriesApi = await axios.get('https://restcountries.com/v3/all');
+    const arrCountriesDB = await arrCountriesApi.data.map(c => {
+        let country = {
+            id: c.cca3,
+            name: c.translations.spa.common,
+            flag: c.flags[1],
+            continent: c.continents.toString(),
+            capital: c.capital.toString(),
+            subregion: c.subregion,
+            area: c.area,
+            population: c.population
+        }
+        return country
+    });
+    return arrCountriesDB
+}
+
+// GET / countries:
+// En una primera instancia deberán traer todos los países desde restcountries y guardarlos en su propia base de datos y luego ya utilizarlos desde allí(Debe retonar sólo los datos necesarios para la ruta principal)
+// Obtener un listado de los paises.
+
+router.get('/')
 
 
 module.exports = router;
